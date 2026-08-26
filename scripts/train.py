edce0a713cbb416bedd2f739120d64bfe8455335
Chakir Benlafkih <chakir.benlafkih@gmail.com>
2026-08-26 12:09:03 +0100

    feature importance

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,6 @@
 import os
 import joblib
+from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.compose import make_column_transformer, make_column_selector
 from sklearn.ensemble import RandomForestClassifier
@@ -8,6 +9,7 @@ from sklearn.pipeline import make_pipeline
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from generate_L_curves import  plot_learning_curve
+from feature_importance import plot_feature_importance
 
 X_train = pd.read_csv("../data/X_train_processed.csv")
 y_train = pd.read_csv("../data/y_train.csv").squeeze()
@@ -32,6 +34,7 @@ preprocessor = make_column_transformer(
     ),
     remainder="passthrough"
 )
+
 
 X_train_encoded = preprocessor.fit_transform(X_train)
 X_val_encoded = preprocessor.transform(X_val)
@@ -61,7 +64,13 @@ model.fit(
 )
 
 print("Training completed.")
+os.makedirs("../results/model", exist_ok=True)
 
+plot_feature_importance(
+    model,
+    preprocessor,
+    "../results/model/feature_importance.png"
+)
 
 plot_learning_curve(
     model,
@@ -70,7 +79,6 @@ plot_learning_curve(
 
 
 # Save model + feature columns
-os.makedirs("../results/model", exist_ok=True)
 
 model_data = {"preprocessor": preprocessor,"model": model}
 joblib.dump(model_data,"../results/model/xgboost.pkl")
